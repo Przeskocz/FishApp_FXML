88d7a173a67acfa56877acf0cdef78c4aeaf029f
JoannaZ <johanna.95@o2.pl>
2019-11-15 19:35:41 +0100

    Implement list of 'availableFish' in GUI.

--- src/app/PiscaryPanelController.java
+++ src/app/PiscaryPanelController.java
@@ -1,7 +1,11 @@
 package app;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import model.Fish;
 import model.Piscary;
 
@@ -28,6 +32,8 @@ public class PiscaryPanelController {
     Label labelCountRod;
     @FXML
     Label labelIsBookingSlot;
+    @FXML
+    ListView<String> listViewAvailableFish;
 
     private Piscary onePiscary;
 
@@ -35,6 +41,17 @@ public class PiscaryPanelController {
         String[] baits = {"kulki proteinowe słodkie", "kulki proteinowe śmierdzące", "robaczek", "kukurydza"};
         onePiscary = new Piscary(5.0, "Nad Stawami", "Siedliska 10", "+48 754 220 272",
                 new HashSet<Fish>(), 7, 19, 30, 30, 3, true, baits);
+
+        //dodawanie fiszków do stawu
+        onePiscary.addNewFish(new Fish("Karp", Fish.TypeOfFish.HERBIVORE, 1, 25));
+        onePiscary.addNewFish(new Fish("Amur", Fish.TypeOfFish.HERBIVORE, 1, 15));
+        onePiscary.addNewFish(new Fish("Jesiotr", Fish.TypeOfFish.HERBIVORE, 2, 15));
+        onePiscary.addNewFish(new Fish("Szczupak", Fish.TypeOfFish.MEAT_EATER, 1, 10));
+        onePiscary.addNewFish(new Fish("Karaś", Fish.TypeOfFish.HERBIVORE, 1, 3));
+        onePiscary.addNewFish(new Fish("Płoć", Fish.TypeOfFish.HERBIVORE, 1, 2));
+        onePiscary.addNewFish(new Fish("Okoń", Fish.TypeOfFish.MEAT_EATER, 0, 1));
+        onePiscary.addNewFish(new Fish("Sum", Fish.TypeOfFish.SCAVENGER, 5, 40));
+
 
         //TODO: please implemet list of 'availableFish' and array of 'effectiveBait' in GUI Piscary
     }
@@ -51,5 +68,15 @@ public class PiscaryPanelController {
         labelPriceNight.setText(String.valueOf(onePiscary.getPriceNight())+ " zł");
         labelCountRod.setText(String.valueOf(onePiscary.getCountRod())+ " szt.");
         labelIsBookingSlot.setText(onePiscary.isBookingSlot() ? "Tak" : "Nie"); //wyciagam boolean
+
+        ObservableList<String> speciesOfFish = FXCollections.observableArrayList();
+
+        for (Fish item : onePiscary.getAvailableFish()) {
+            String speciesFish = item.getSpecies();
+            speciesOfFish.add(speciesFish); //dodanie gatunki fiszki do obserwowalnej listy
+        }
+
+        listViewAvailableFish.setItems(speciesOfFish);// wstawienie obserwowalnej listy do kontrolki listView
+
     }
 }
